851d49d33ae927c82f459306a08e0599467360a0
Zoey <zoey@panties.moe>
2022-01-18 20:34:32 -0500

    [TokenLogger] Add plugin info

--- TokenLogger/rollup.config.ts
+++ TokenLogger/rollup.config.ts
@@ -1,19 +1,38 @@
 import { defineConfig } from "rollup";
+import { basename } from "path";
+import { writeFileSync } from "fs";
 import esbuild from "rollup-plugin-esbuild";
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 
-import { basename } from "path";
 const pluginName = basename(process.cwd());
 
 export default defineConfig({
     input: "src/index.ts",
     output: [
-        { file: `dist/${pluginName}.js`, format: "cjs", strict: false},
+        { file: `dist/${pluginName}.js`, format: "cjs", strict: false },
     ],
     plugins: [
         nodeResolve(),
         commonjs(),
-        esbuild({ minify: true, target: "es2015", }),
+        esbuild({ minify: true, target: "es2015" }),
+        createPluginJson(),
     ]
 });
+
+function createPluginJson() {
+    return {
+      name: 'plugin-info',
+      resolveId(source) {
+        const info = require('./package.json');
+        const data = {
+            "name": pluginName,
+            "description": info.description ?? "No description was provided.",
+            "author": info.author.name ?? "Unknown",
+            "version": info.version ?? "1.0.0"
+        };
+
+        writeFileSync(`dist/${pluginName}.json`, JSON.stringify(data, null, "\t"));
+      },
+    };
+  }
