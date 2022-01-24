89562210e46f4935dd52f7263a753d22d5687601
Zoey <zoey@panties.moe>
2022-01-23 22:50:46 -0500

    [EnableStaging] Now hook hasFlag to enable experiments

--- EnableStaging/src/index.ts
+++ EnableStaging/src/index.ts
@@ -1,1 +1,9 @@
-window["GLOBAL_ENV"]["RELEASE_CHANNEL"] = "staging";
+import { getModule } from "aliucord-api/dist/modules/module";
+
+const userRecord = getModule(m => m.default?.name === "UserRecord");
+
+const _hasFlag = userRecord.default.prototype.hasFlag;
+userRecord.default.prototype.hasFlag = function(t) {
+  if (t === 1) return true;
+  return _hasFlag.call(this, t);
+}
