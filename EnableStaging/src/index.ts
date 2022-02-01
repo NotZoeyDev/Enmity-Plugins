6137194644af051e3ccb4fdaaacf2409b28eb7f8
Zoey <zoey@panties.moe>
2022-01-31 19:15:18 -0500

    [EnableStaging] Now using the new patcher

--- EnableStaging/src/index.ts
+++ EnableStaging/src/index.ts
@@ -1,9 +1,10 @@
 import { getModule } from "aliucord-api/dist/modules/module";
+import { patchInstead } from "aliucord-api/dist/modules/patcher";
 
 const userRecord = getModule(m => m.default?.name === "UserRecord");
 
-const _hasFlag = userRecord.default.prototype.hasFlag;
-userRecord.default.prototype.hasFlag = function(t) {
-  if (t === 1) return true;
-  return _hasFlag.call(this, t);
-}
+patchInstead("enable-staging", userRecord.default.prototype, "hasFlag", (self, args, res) => {
+  if (args[0] === 1) return true;
+
+  return res(args[0]);
+});
