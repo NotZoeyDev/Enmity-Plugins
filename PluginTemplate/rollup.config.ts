3f1b52d2779753964e036b5a56546a2ba0d72903
Zoey <zoey@panties.moe>
2022-02-10 14:20:48 -0500

    [PluginTemplate] Use rollup-plugin-cjs-es instead of commonjs plugin

--- PluginTemplate/rollup.config.ts
+++ PluginTemplate/rollup.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from "rollup";
 import { basename } from "path";
 import { writeFileSync } from "fs";
 import esbuild from "rollup-plugin-esbuild";
-import commonjs from "@rollup/plugin-commonjs";
+import cjs from "rollup-plugin-cjs-es";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 
 const pluginName = basename(process.cwd());
@@ -14,25 +14,25 @@ export default defineConfig({
     ],
     plugins: [
         nodeResolve(),
-        commonjs(),
+        cjs(),
         esbuild({ minify: true, target: "ES2019" }),
         createPluginJson(),
     ]
 });
 
 function createPluginJson() {
-  return {
-    name: 'plugin-info',
-    resolveId(source) {
-      const info = require('./package.json');
-      const data = {
-          "name": pluginName,
-          "description": info?.description ?? "No description was provided.",
-          "author": info?.author?.name ?? "Unknown",
-          "version": info?.version ?? "1.0.0"
-      };
+    return {
+      name: 'plugin-info',
+      resolveId(source) {
+        const info = require('./package.json');
+        const data = {
+            "name": pluginName,
+            "description": info?.description ?? "No description was provided.",
+            "author": info?.author?.name ?? "Unknown",
+            "version": info?.version ?? "1.0.0"
+        };
 
-      writeFileSync(`dist/${pluginName}.json`, JSON.stringify(data, null, "\t"));
-    }
-  };
+        writeFileSync(`dist/${pluginName}.json`, JSON.stringify(data, null, "\t"));
+      }
+    };
 }
