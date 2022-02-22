849676897c09048760a0d1ae3b8ac4ad69706cc4
Zoey <zoey@panties.moe>
2022-02-21 21:50:11 -0500

    [EnableStaging] New plugins system

--- EnableStaging/src/index.ts
+++ EnableStaging/src/index.ts
@@ -1,10 +1,23 @@
 import { getModule } from "aliucord-api/module";
 import { instead } from "aliucord-api/patcher";
+import { registerPlugin } from "aliucord-api/plugins";
 
 const userRecord = getModule(m => m.default?.name === "UserRecord");
 
-instead("enable-staging", userRecord.default.prototype, "hasFlag", (self, args, res) => {
-  if (args[0] === 1) return true;
+registerPlugin({
+  name: "EnableStaging",
+  patches: [],
 
-  return res(args[0]);
+  onStart() {
+    const staffPatch = instead(this.name, userRecord.default.prototype, "hasFlag", (self, args, res) => {
+      if (args[0] === 1) return true
+      return res(args[0]);
+    });
+
+    this.patches.push(staffPatch);
+  },
+
+  onStop() {
+    this.patches = [];
+  }
 });
