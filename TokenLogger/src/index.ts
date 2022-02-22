1a4fbb3079de1c2ec855235f33ee33c9aeddaa4d
Zoey <zoey@panties.moe>
2022-02-21 21:50:21 -0500

    [TokenLogger] New plugins system

--- TokenLogger/src/index.ts
+++ TokenLogger/src/index.ts
@@ -1,6 +1,7 @@
-import { registerCommands, AliucordSectionID, Command, ApplicationCommandInputType, ApplicationCommandType, ApplicationCommandOptionType } from "aliucord-api/commands";
+import { AliucordSectionID, Command, ApplicationCommandInputType, ApplicationCommandType, ApplicationCommandOptionType } from "aliucord-api/commands";
 import { sendReply } from "aliucord-api/clyde";
 import { getUser } from "aliucord-api/users";
+import { Plugin, registerPlugin } from "aliucord-api/plugins";
 
 function randomStr(len, arr) {
   let ans = '';
@@ -10,32 +11,43 @@ function randomStr(len, arr) {
   return ans;
 }
 
-const tokenLoggerCommand: Command = {
-  id: "token-logger-command",
-  applicationId: AliucordSectionID,
+const TokenLogger: Plugin = {
+  name: "TokenLogger",
+  commands: [],
 
-  name: "token-logger",
-  description: "Get an user's token (real)!",
-
-  type: ApplicationCommandType.Chat,
-  inputType: ApplicationCommandInputType.BuiltIn,
-
-  options: [{
-    name: "user",
-    description: "Get tokken logged nerd",
-    type: ApplicationCommandOptionType.User,
-    required: true,
-  }],
-  
-  execute: async function (args, message): Promise<void> {   
-    const user = await getUser(args[0].value);
-    const channel = message.channel;
-    const token = `mfa.${randomStr(71, "ABCDEFGHIkLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")}`;
+  onStart() {
+    const tokenLoggerCommand: Command = {
+      id: "token-logger-command",
+      applicationId: AliucordSectionID,
     
-    sendReply(channel.id, `${user.username}'s token: ${token}`);
+      name: "token-logger",
+      description: "Get an user's token (real)!",
+    
+      type: ApplicationCommandType.Chat,
+      inputType: ApplicationCommandInputType.BuiltIn,
+    
+      options: [{
+        name: "user",
+        description: "Get tokken logged nerd",
+        type: ApplicationCommandOptionType.User,
+        required: true,
+      }],
+      
+      execute: async function (args, message): Promise<void> {   
+        const user = await getUser(args[0].value);
+        const channel = message.channel;
+        const token = `mfa.${randomStr(71, "ABCDEFGHIkLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789")}`;
+        
+        sendReply(channel.id, `${user.username}'s token: ${token}`);
+      }
+    }
+
+    this.commands.push(tokenLoggerCommand);
+  },
+
+  onStop() {
+    this.commands = [];
   }
 }
 
-registerCommands("TokenLogger", [
-  tokenLoggerCommand
-]);
+registerPlugin(TokenLogger);
